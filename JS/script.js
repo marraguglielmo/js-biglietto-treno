d19b694300ed389f3d1617ad916f3f47e9b778b8
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-20 15:10:34 +0100

    [add] script

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const km = parseInt(prompt("Inserisci i km da percorrere"));
+const eta = parseInt(prompt("Inserisci l'età del passeggero"));
+const rate = 0.21;
+
+let ticket = km * rate;
+
+if(eta < 18){
+    ticket -= ticket * 0.20;
+}else if(eta > 65){
+    ticket -= ticket *0.40
+}
+ticket = ticket.toFixed(2);
+
+document.getElementById('output').innerHTML += ticket;
+
+console.log(km, eta, ticket);
